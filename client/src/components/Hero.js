7b5bf59a423f01190436199ee70e0dead583c5b2
Alan Balcom <alanbalcom@Alans-MacBook-Pro.local>
2021-08-10 22:56:24 -0400

    user inputs and fade in animation

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { FloatingLabel, Form, Row, Container } from 'react-bootstrap';
 
 
 function Hero() {
   return (
       
-    <div className='hero'></div>
+    <div className='hero'>
+    <Container className='container'>
+    <Row className="g-2">
+    
+      <FloatingLabel controlId="floatingInputGrid" label="Search by Cocktail">
+        <Form.Control className='userInput' type="input" placeholder="name@example.com" />
+      </FloatingLabel>
+    
+      <FloatingLabel controlId="floatingSelectGrid" label="Search by Ingredients">
+        <Form.Select aria-label="Floating label select example" className='userInput'>
+          <option>Search by Ingredients</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </Form.Select>
+      </FloatingLabel>
+   
+    </Row>
+    </Container>
+    </div>
     
   );
 }
